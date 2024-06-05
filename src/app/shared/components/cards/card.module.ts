347e5b68fc380a5fd5e52b885a66fa57194f554c
Fernanda Guimaraes Salgado - Wayon <fsalgado.wayon@veloe.com.br>
2024-06-04 21:49:14 -0300

    style: remover estilo do scroll

--- src/app/shared/components/cards/card.module.ts
+++ src/app/shared/components/cards/card.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { CardListComponent } from './card-list/card-list.component';
 import { NgIconsModule } from '@ng-icons/core';
 import { HeroCake, HeroInbox, HeroShoppingCart } from '@ng-icons/heroicons/outline';
+
 import { ButtonModule } from '../button/button.module';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   imports: [
@@ -12,6 +14,7 @@ import { ButtonModule } from '../button/button.module';
 
     NgIconsModule,
     NgIconsModule.withIcons({ HeroInbox, HeroShoppingCart, HeroCake }),
+    PipesModule
   ],
   declarations: [CardListComponent],
   exports: [CardListComponent],
